8a60447909754379ad0e7eb65644f56a51535b98
Nam <hungpig83@gmail.com>
2023-07-05 11:39:40 +0700

    travis CI

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'amrz3joeec'
+const apiId = '0mjrmiea9l'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
@@ -7,5 +7,5 @@ export const authConfig = {
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-cl086mx47plg6xzl.us.auth0.com',            // Auth0 domain
   clientId: 'L62gr0kvrB8uD2nn6psxpwYoOMU0t9f1',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://serverless-c4-todo-images-hungnn-frontend-dev.s3-website-us-east-1.amazonaws.com/callback'
 }
